affa95f7708a412ae0d6a739486d02998cd2f0a4
Miguel <Miguel@DESKTOP-0MUMB30>
2022-12-20 18:42:09 +0100

    cambio de Hello a Saludos

--- ProyectoGitRamasEjemplo/src/main.java
+++ ProyectoGitRamasEjemplo/src/main.java
@@ -1,12 +1,12 @@
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello mundo");
+		System.out.println("Saludos mundo");
 		imprimeSaludo();
 	}
 	
 	public static void imprimeSaludo() { 
-		System.out.println(“Hei”);	
+		System.out.println("Hei");	
 	}
 
 }
