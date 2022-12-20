0952cd607549b91e5bd30c077ddf55019413656b
Miguel Angel Plaza Paredes <118214636+MiguelAngelPlazaParedes@users.noreply.github.com>
2022-12-20 18:26:06 +0100

    añado metodo imprimeSaludo

--- ProyectoGitRamasEjemplo/src/main.java
+++ ProyectoGitRamasEjemplo/src/main.java
@@ -3,5 +3,10 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
 	}
+	
+	public static void imprimeSaludo() { 
+		System.out.println(“Hei”);	
+	}
+
 }
 
